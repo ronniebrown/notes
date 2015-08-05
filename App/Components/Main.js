c20c48f349e5308f341155c767acc758cb4cfa9f
Ronnie Brown <ronnievoice@gmail.com>
2015-08-04 23:02:41 -0700

    add search input and search button

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -54,10 +54,40 @@ var styles = StyleSheet.create({
 });
 
 class Main extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      isLoading: false,
+      error: false
+    }
+  }
+  handleChange(event) {
+    this.setState({
+      username: event.nativeEvent.text
+    });
+  }
+  handleSubmit() {
+    this.setState({
+      isLoading: true
+    });
+    console.log('SUBMIT', this.state.username);
+  }
   render() {
     return(
       <View style={styles.mainContainer}>
-        <Text> Test Test </Text>
+        <Text style={styles.title}> Search for a Github User </Text>
+        <TextInput
+          style={styles.searchInput}
+          value={this.state.username}
+          onChange={this.handleChange.bind(this)} />
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleSubmit.bind(this)}
+          underlayColor="white">
+            <Text style={styles.buttonText}> Search </Text>
+          </TouchableHighlight>
       </View>
     );
   }
