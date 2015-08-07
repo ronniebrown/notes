e3a4f38d962efe86c35a7e03f79bf797e035d8d0
Ronnie Brown <ronnievoice@gmail.com>
2015-08-07 16:02:27 -0700

    finish routes

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -1,13 +1,15 @@
 var React = require('react-native');
 var Profile = require('./Profile');
+var api = require('../Utils/api');
 var Repositories = require('./Repositories');
-var api = require('../Utils/api')
+var Notes = require('./Notes');
 
 var {
   Text,
   View,
-  StyleSheet,
+  NavigatorIOS,
   Image,
+  StyleSheet,
   TouchableHighlight
 } = React;
 
@@ -17,7 +19,7 @@ var styles = StyleSheet.create({
     flex: 1
   },
   image: {
-    height: 350
+    height: 350,
   },
   buttonText: {
     fontSize: 24,
@@ -26,73 +28,87 @@ var styles = StyleSheet.create({
   }
 });
 
-class Dashboard extends React.Component {
-  makeBackground(btn) {
+class Dashboard extends React.Component{
+  makeBackground(btn){
     var obj = {
       flexDirection: 'row',
       alignSelf: 'stretch',
       justifyContent: 'center',
       flex: 1
     }
-
-    if (btn === 0) {
+    if(btn === 0){
       obj.backgroundColor = '#48BBEC';
-    } else if (btn === 1) {
-      obj.backgroundColor = '#E77AAE';      
+    } else if (btn === 1){
+      obj.backgroundColor = '#E77AAE';
     } else {
       obj.backgroundColor = '#758BF4';
     }
-
     return obj;
   }
-  goToProfile() {
+  goToProfile(){
     this.props.navigator.push({
       component: Profile,
       title: 'Profile Page',
       passProps: {userInfo: this.props.userInfo}
-    });
+    })
   }
-  goToRepos() {
+  goToRepos(){
     api.getRepos(this.props.userInfo.login)
-      .then((res) => {    
+      .then((jsonRes) => {
         this.props.navigator.push({
           component: Repositories,
-          title: 'Repos',
+          title: "Repositories Page",
           passProps: {
-            userInfo: this.props.userInfo,
-            repos: res
+            repos: jsonRes,
+            userInfo: this.props.userInfo
+          }
+        });
+      })
+  }
+  goToNotes(){
+    api.getNotes(this.props.userInfo.login)
+      .then((jsonRes) => {
+        jsonRes = jsonRes || {};
+        this.props.navigator.push({
+          component: Notes,
+          title: 'Notes',
+          passProps: {
+            notes: jsonRes,
+            userInfo: this.props.userInfo
           }
         });
       });
   }
-  goToNotes() {
-    console.log('notes page')
-  }
-  render() {
+  render(){
     return (
       <View style={styles.container}>
-        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image}/>
         <TouchableHighlight
-          style={this.makeBackground(0)}
-          onPress={this.goToProfile.bind(this)}
-          underlayColor="#88D4F5">
-            <Text style={styles.buttonText}> View Profile </Text>
+            style={this.makeBackground(0)}
+            onPress={this.goToProfile.bind(this)}
+            underlayColor="#88D4F5">
+              <Text style={styles.buttonText}>View Profile</Text>
         </TouchableHighlight>
         <TouchableHighlight
-          style={this.makeBackground(1)}
-          onPress={this.goToRepos.bind(this)}
-          underlayColor="#88D4F5">
-            <Text style={styles.buttonText}> View Repos </Text>
+            style={this.makeBackground(1)}
+            onPress={this.goToRepos.bind(this)}
+            underlayColor="#E39EBF">
+              <Text style={styles.buttonText}>View Repositories</Text>
         </TouchableHighlight>
         <TouchableHighlight
-          style={this.makeBackground(2)}
-          onPress={this.goToNotes.bind(this)}
-          underlayColor="#88D4F5">
-            <Text style={styles.buttonText}> View Notes </Text>
+            style={this.makeBackground(2)}
+            onPress={this.goToNotes.bind(this)}
+            underlayColor="#9BAAF3">
+              <Text style={styles.buttonText}>Take Notes</Text>
         </TouchableHighlight>
       </View>
-    );
+    )
   }
 };
 
+Dashboard.propTypes = {
+  userInfo: React.PropTypes.object.isRequired
+}
+
 module.exports = Dashboard;
+
--- App/Components/Notes.js
+++ App/Components/Notes.js
@@ -15,7 +15,7 @@ var {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
   buttonText: {
     fontSize: 18,
@@ -36,7 +36,7 @@ var styles = StyleSheet.create({
     flex: 10
   },
   rowContainer: {
-    padding: 10
+    padding: 10,
   },
   footerContainer: {
     backgroundColor: '#E3E3E3',
@@ -46,40 +46,40 @@ var styles = StyleSheet.create({
 });
 
 class Notes extends React.Component{
-  constructor() {
-    super(props)
-    this.ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2});
+  constructor(props){
+    super(props);
+    this.ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
     this.state = {
       dataSource: this.ds.cloneWithRows(this.props.notes),
       note: '',
       error: ''
     }
   }
-  handleChange(e) {
+  handleChange(e){
     this.setState({
       note: e.nativeEvent.text
-    });
+    })
   }
-  handleSubmit(e) {
+  handleSubmit(){
     var note = this.state.note;
     this.setState({
       note: ''
-    })
-
+    });
     api.addNote(this.props.userInfo.login, note)
       .then((data) => {
         api.getNotes(this.props.userInfo.login)
           .then((data) => {
             this.setState({
-              dataSource: this.ds.cloneWithRows(data);
+              dataSource: this.ds.cloneWithRows(data)
             })
-          })
-      }).catch((err) => {
-        console.log('Request failed', err);
+          });
+      })
+      .catch((error) => {
+        console.log('Request failed', error);
         this.setState({error})
       });
   }
-  renderRow(row) {
+  renderRow(rowData){
     return (
       <View>
         <View style={styles.rowContainer}>
@@ -89,33 +89,35 @@ class Notes extends React.Component{
       </View>
     )
   }
-  footer() {
+  footer(){
     return (
       <View style={styles.footerContainer}>
         <TextInput
-          style={styles.searchInput}
-          value={this.state.note}
-          onChange={this.handleChange.bind(this)}
-          placeholder="New Note" />
+            style={styles.searchInput}
+            value={this.state.note}
+            onChange={this.handleChange.bind(this)}
+            placeholder="New Note" />
         <TouchableHighlight
-          style={style.button}
-          onPress={this.handleSubmit.bind(this)}
-          underlayColor="#88D4F5">
-            <Text style={styles.buttonText}> Sub </Text>
-        </TouchableHighlight>
+            style={styles.button}
+            onPress={this.handleSubmit.bind(this)}
+            underlayColor="#88D4F5">
+              <Text style={styles.buttonText}>Submit</Text>
+          </TouchableHighlight>
       </View>
     )
   }
-  render() {
-    <View style={styles.container}>
-      <ListView
-        dataSource={this.state.dataSource}
-        render={this.renderRow}
-        renderHeader={() => <Badge userInfo={this.props.userInfo}/> } />
-      {this.footer()}
-    </View>
+  render(){
+    return (
+      <View style={styles.container}>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow}
+            renderHeader={() => <Badge userInfo={this.props.userInfo}/>} />
+        {this.footer()}
+      </View>
+    )
   }
-}
+};
 
 Notes.propTypes = {
   userInfo: React.PropTypes.object.isRequired,
